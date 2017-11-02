868a78c089827ffd9c81d60c06c00308f14ca76b
Sam Mercer <sam@mercerfamily.org.uk>
2017-11-02 19:56:27 +0000

    Multiprocessing working
    - currently spits files into working directory not correct one. I think is mac issue will look into later

--- get.py
+++ get.py
@@ -1,8 +1,9 @@
 import urllib.request
 import os
 import shutil
+from multiprocessing import Process
 x = 0
-counter = 0
+procs = []
 
 
 def append_ids(url, name):
@@ -10,27 +11,32 @@ def append_ids(url, name):
 
 
 def get_images(name, path):
-    x = 0
     if os.path.isdir(path):
         shutil.rmtree(path)
         os.mkdir(path)
     else:
         os.mkdir(path)
-    print(path)
+
     with open(name, encoding="utf8") as file:
         # print(file)
         for line in file:
-            # print(line)
-            filename = str(x) + ".jpg"
-            fullfilename = path + "\\" + filename
-            try:
-                urllib.request.urlretrieve(line, fullfilename)
-                counter += 1
-            except Exception as C:
-                pass
-            x += 1
-    os.remove(name)
+            # request(line,path)
+            proc = Process(target=request, args=(line,path))
+            procs.append(proc)
+            proc.start()
 
+def request(line,path):
+    proc = os.getpid()
+    # print("PID for process is ",proc)
+    line = line.strip()
+    filename = line.split("/")[-1]
+    fullfilename = path + "\\" + filename
+    print(path, fullfilename)
+    try:
+        urllib.request.urlretrieve(line, fullfilename)
+        print("did request for "+ str(line) + " on filename " + str(filename))
+    except Exception as C:
+        print(C)
 
 def return_sub_files(data):
     urllib.request.urlretrieve(
@@ -38,18 +44,14 @@ def return_sub_files(data):
     file2 = open("temp.txt")
     list_temp = file2.readlines()
     if len(list_temp) > 1:
-        # print("Sub sub folder detected", str(data), list_temp)
         for x in list_temp:
-            # response = return_sub_files(x)
             string = x[1:10]
             if string[0] == "n":
-                # print(response)
-                # print("Adding to IDS")
                 file_IDS = open("IDS.txt", mode="a")
                 with open("temp.txt") as file_temp:
                     for line in file_temp:
                         if line[0] == "-":
-                            # print(line, "Was added to IDS.txt")
+                            print(line, "Was added to IDS.txt")
                             file_IDS.write(line)
                 file_IDS.close()
                 return False
@@ -57,7 +59,7 @@ def return_sub_files(data):
         return False
 
 
-# append_ids("http://www.image-net.org/api/text/wordnet.structure.hyponym?wnid=n03544360", "IDS.txt")
+append_ids("http://www.image-net.org/api/text/wordnet.structure.hyponym?wnid=n03544360", "IDS.txt")
 
 with open("IDS.txt") as file:
     for line in file:
@@ -70,8 +72,7 @@ with open("IDS.txt") as file:
                 urllib.request.urlretrieve(
                     "http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=" + string, "images_for_" + string + ".txt")
                 get_images("images_for_" + string + ".txt", string)
-            else:
-                pass
+
 
 
 print(counter, "image files were processed")
